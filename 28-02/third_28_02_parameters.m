584ed9c3424d7d6361744c8e1df33e136c88c8c4
Franco Cicero <fcicero@itba.edu.ar>
2025-02-28 14:17:52 +0100

    -28-02 v2

---
+++ 28-02/third_28_02_parameters.m
@@ -0,0 +1,11 @@
+%% Parameters of the model 1QC Feb 28th
+%%% LOAD
+
+%r: load resistnace [Ohm]
+r=10;
+%l: load inductance [H]
+l=10e-3;
+
+%%%  SWITCHING 
+%fsw: switching frecuency [Hz]
+fSW = 10*1000; 
